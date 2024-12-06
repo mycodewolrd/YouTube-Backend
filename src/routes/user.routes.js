bff838d9a72754cd90d9f47e5594b6756c47f948
Rajnandini <rajnandinipatra2003@gmail.com>
2024-12-07 03:58:50 +0530

    add more routers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -44,11 +44,7 @@ router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
-router
-  .route("/update-avatar")
-  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
-router
-  .route("/update-coverImage")
-  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/update-coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 export default router;
